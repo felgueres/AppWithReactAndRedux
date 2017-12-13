c16981bfadc86791da833f721ed5ba4b5c39eea8
felgueres <pablofelgueres@gmail.com>
2017-12-13 12:04:08 -0800

    'updates'

--- src/index.js
+++ src/index.js
@@ -1,48 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/search_bar';
 import YTSearch from 'youtube-api-search';
-
-// when importing user created files, they need to be imported with the relative path.
-
+import VideoList from './components/video_list';
 const API_KEY = 'AIzaSyAVIbuvBVeNzCLbMeXh6Vt0I6jTsCD3zQk';
 
-// Create new component to produce some html. That what react does.
-// A component is a function that generates html.
-// const means that is a constant in that it will not be reassingeable.
-// Previous Syntax 1.0
-// const App = function(){
-//     return <div>Hi!</div>;
-// }
-
-// ALWAYS CREATE ONE COMPONENT PER FILE!
-
-// To use the YouTube API, the YTSearch takes an object that has a key and term, and a function as second argument.
-
-YTSearch({key: API_KEY, term: 'surfboards'}, function(data){
-	console.log(data);
-});
 
 
-// ES6 syntax 2.0
-const App = () => {
-    return (
-        <div>
-            <SearchBar />
-        </div>
-    );
+class App extends Component {
+	constructor(props){
+		super(props);
+		this.state = { videos: [] };
+
+		YTSearch({key: API_KEY, term: 'surfboards'}, (videos) =>{
+			this.setState({ videos });
+		});
+	}
+
+	render() {
+	    return (
+	        <div>
+	            <SearchBar />
+	            <VideoList />
+	        </div>
+	    );
+    }
 }
 
-
-
 // 
-// Note both syntaxes are basically the same, there is only a distinction in terms of Javascript's "this".
 
-// Components don't put them directly in the DOM. Take the component that generates HTML and put it in the DOM.
+ReactDOM.render(<App />, document.querySelector('.container'))
 
-// Note that calling App is just calling the class App. You need JSX to create an instance of the class -- eg. <App />
 
-ReactDOM.render(<App />, document.querySelector('.container'));
+// ES6 Syntax: 
+// Whenever you have the key and value from a dict the same, you can refactor it with:
+// this.setState({videos: videos}) this is the same as this.setState({ videos });
 
-// Notice that when you pass the app instance to the dom, you need to specify where to render it. That is the second argument of
-// the function render; in this case we are selecting the class container that is in the index html file.
+
