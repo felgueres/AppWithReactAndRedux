a1b883871ec5c8e4ddd7ab46203defb1285bc648
felgueres <pablofelgueres@gmail.com>
2017-12-13 14:24:37 -0800

    'handling_null_props'

--- src/components/video_detail.js
+++ src/components/video_detail.js
@@ -2,13 +2,17 @@ import React from 'react';
 
 const VideoDetail = ({video}) =>{
 
+  // to avoid running this function before the video has been fetched, let's do some logic.
+
+  if (!video){
+    return <div>Loading</div>
+  };
+
   const videoId = video.id.videoId;
   // const url = 'https://www.youtube.com/embed'+ videoId;
 // Sugar syntax for the line above.
   const url = `https://www.youtube.com/embed/${videoId}`;
-
-
-  return {
+  return (
     <div className='video-detail col-md-8'>
       <div className='embed-responsive embed-responsive-16by9'>
         <iframe className='embed-responsive-item' src={url}></iframe>
@@ -18,7 +22,7 @@ const VideoDetail = ({video}) =>{
         <div>{video.snippet.description}</div>
       </div>
     </div>
-  };
+  );
 };
 
 export default VideoDetail;
--- src/index.js
+++ src/index.js
@@ -6,6 +6,9 @@ import VideoList from './components/video_list';
 import VideoDetail from './components/video_detail';
 const API_KEY = 'AIzaSyAVIbuvBVeNzCLbMeXh6Vt0I6jTsCD3zQk';
 
+// NOTE: VERY IMPORTANT: React will try to render an element that may still be in process of being obtained, ie. request, so you need to set some conditions to avoid errors of "missing elements" to be called.
+// This is essentially how you handle null props.
+
 class App extends Component {
 	constructor(props){
 		super(props);
