02b437f01866e91361315fc85cfa1a8afbe69dd0
felgueres <pablofelgueres@gmail.com>
2017-12-13 13:36:48 -0800

    'listComponents'

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -45,7 +45,7 @@ import React, { Component } from 'react';
 class SearchBar extends Component {
     constructor(props){
         super(props);
-        this.state = {term: 'Connected'};
+        this.state = {term: 'Nothing Here'};
     }
 
     render(){
--- src/index.js
+++ src/index.js
@@ -5,14 +5,12 @@ import YTSearch from 'youtube-api-search';
 import VideoList from './components/video_list';
 const API_KEY = 'AIzaSyAVIbuvBVeNzCLbMeXh6Vt0I6jTsCD3zQk';
 
-
-
 class App extends Component {
 	constructor(props){
 		super(props);
 		this.state = { videos: [] };
 
-		YTSearch({key: API_KEY, term: 'surfboards'}, (videos) =>{
+		YTSearch({key: API_KEY, term: 'photovoltaics'}, (videos) => {
 			this.setState({ videos });
 		});
 	}
@@ -21,7 +19,7 @@ class App extends Component {
 	    return (
 	        <div>
 	            <SearchBar />
-	            <VideoList />
+	            <VideoList videos={this.state.videos} />
 	        </div>
 	    );
     }
@@ -36,4 +34,9 @@ ReactDOM.render(<App />, document.querySelector('.container'))
 // Whenever you have the key and value from a dict the same, you can refactor it with:
 // this.setState({videos: videos}) this is the same as this.setState({ videos });
 
-
+// To pass from parent to child, you pass a property with JSX on the tag of the element.
+// This is referred to as passing props.
+// In this case you are passing data from App to Video lists: You are passing prop videos to VideoList
+// If it is a functional component -- like VideoList here -- it's essentially passed as argument to the function.
+// Example: 
+// <VideoList videos= {this.state.videos} />
